2178add474015307a23aee9142427e7fd8cf842f
afrian <octavarium07@gmail.com>
2020-08-24 17:59:38 +0700

    add test

--- users/user.py
+++ users/user.py
@@ -1,32 +1,43 @@
-from pyunitreport import HTMLTestRunner
+import HtmlTestRunner
 import requests
 import unittest
+import logging
+
+logging.basicConfig(filename="users.log", level=logging.DEBUG)
 
 
 class User(unittest.TestCase):
 
     def test_get_user_by_id(self):
+        """test_get_user_by_id"""
         customer_id = 1
         print('getting user by id : ' + str(customer_id))
         # suppose customer id is added in url
         url = "https://run.mocky.io/v3/233c2093-e1c0-4bad-85ad-73b19322e593?customer_id" + str(customer_id)
         response = requests.get(url)
+        assert 200 == response.status_code
         if response.status_code == 200:
-
             self.print_response(response)
 
-    def print_response(self, response):
+    @staticmethod
+    def print_response(response):
         print(response.json())
+        logging.info(response.json())
         # get json by it's attribute
         print(response.json()['id'])
+        logging.debug(response.json()['id'])
 
-    def test_get_users(self):
+    @staticmethod
+    def test_get_users():
+        """test_get_users"""
         print('getting list of users')
         url = "https://run.mocky.io/v3/4bec55e4-a566-4422-abfc-3c5aecd85064"
         response = requests.get(url)
+        assert 200 == response.status_code
         if response.status_code == 200:
             print(response.json())
+            logging.info(response.json())
 
 
 if __name__ == '__main__':
-    unittest.main(testRunner=HTMLTestRunner(output='user-test'))
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner())
